c3f62acae6bbc5e612ff1cbd26ad606284e1eb5e
PASOKON\obich <obichan8670@gmail.com>
2025-07-07 17:51:12 +0900

    スキルカードの選択途中

--- Assets/Script/BattleSystem/BattleManager.cs
+++ Assets/Script/BattleSystem/BattleManager.cs
@@ -9,8 +9,14 @@ public class BattleManager : MonoBehaviour
     [SerializeField] CardController cardPrefab;
     [SerializeField] Transform PlayerHandTransform;
     [SerializeField] Dictionary<int, int> deck;
+
+    List<int> handcard;
+    List<CardController> card;
     public int deckcardid;
     public Transform hand;
+    public GameObject check;
+    bool hand1, hand2, hand3;
+    int limitcard = 0;
 
     void Start()
     {
@@ -68,27 +74,72 @@ public class BattleManager : MonoBehaviour
         TurnCalc();
     }
 
-    void CreateCard(Transform hand)
+    IEnumerator CreateCard(Transform hand)
     {
+        //三枚提示
         for(int i = 0; i < 3; i++)
         {
             int draw = Random.Range(0, 41);
-            CardController card = Instantiate(cardPrefab, hand, false);
-            card.Init(draw);
-            deck.Remove(i);
+            card[i] = Instantiate(cardPrefab, hand, false);
+            deck.Remove(draw);
+            handcard[i] = draw;
         }
+
+        //選択し、選択されたものにチェックを表示する
         if(Input.GetKey(KeyCode.Alpha1))
         {
-
+            if (limitcard < 2)
+            {
+                hand1 = !hand1;
+                Instantiate(check, hand, hand1);
+            }
+            hand1 = !hand1;
+            if(hand1)
+            {
+                limitcard += 1;
+            }
+            else 
+            {
+                limitcard -= 1;
+            }
+                Instantiate(check, hand, hand1);
         }
         else if(Input.GetKey(KeyCode.Alpha2))
         {
+            if(limitcard < 2)
+            {
+                hand2 = !hand2;
+                Instantiate(check, hand, hand2);
+            }
 
+            if (hand2)
+            {
+                limitcard += 1;
+            }
+            else
+            {
+                limitcard -= 1;
+            }
         }
         else if(Input.GetKey(KeyCode.Alpha3))
         {
+            if (limitcard < 2)
+            {
+                hand3 = !hand3;
+                Instantiate(check, hand, hand3);
+            }
 
+            if (hand3)
+            {
+                limitcard += 1;
+            }
+            else
+            {
+                limitcard -= 1;
+            }
         }
+        yield return new WaitForSeconds(10f);
 
+        yield break ;
     }
 }
