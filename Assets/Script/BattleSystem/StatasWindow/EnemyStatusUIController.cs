2b1c813d77a811614f4a7e172d42e1b91602b1c7
PASOKON\obich <obichan8670@gmail.com>
2025-10-08 15:27:36 +0900

    プッシュ

---
+++ Assets/Script/BattleSystem/StatasWindow/EnemyStatusUIController.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyStatusUIController : MonoBehaviour
+{
+    [SerializeField] private Image characterIcon;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI defenseText;
+    [SerializeField] private Slider hpSlider;
+
+    /// <summary>
+    /// Enemyのデータを使ってUIを初期設定する
+    /// </summary>
+    public void SetEnemyStatus(EnemyModel enemy)
+    {
+        nameText.text = enemy.EnemyName;
+        attackText.text = enemy.EnemyAttackPower.ToString();
+        defenseText.text = enemy.EnemyDefensePower.ToString();
+
+        // 敵にはレベルがないので、レベル表示を非表示にする
+        if (levelText != null)
+        {
+            levelText.gameObject.SetActive(false);
+        }
+
+        if (enemy.EnemySprite != null)
+        {
+            characterIcon.sprite = enemy.EnemySprite;
+        }
+
+        hpSlider.maxValue = enemy.EnemyHP;
+        hpSlider.value = enemy.EnemyHP;
+    }
+    /// <summary>
+    /// HPバーの表示を更新する
+    /// </summary>
+    public void UpdateHP(float currentHP)
+    {
+        hpSlider.value = currentHP;
+    }
+}
